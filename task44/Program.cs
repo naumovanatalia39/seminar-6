e66976a41b1ff49213986d85f1d4549fd665e921
naumovanatalia <naumova.na43@mail.ru>
2022-12-18 13:28:29 +0200

    add task44

---
+++ task44/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Введите число");
+int N = Convert.ToInt32(Console.ReadLine());
+int [] fib = new int [N];
+fib [0] = 0;
+fib [1] = 1;
+for(int i = 2; i < N; i++)
+{
+    fib [i] = fib[i - 1] + fib[i - 2];
+}
+Console.WriteLine($"{string.Join(" ,", fib)}");
