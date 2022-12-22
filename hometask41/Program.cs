81c95268fb54938c36c1250c4525bb3bad5d982a
naumovanatalia <naumova.na43@mail.ru>
2022-12-22 18:17:41 +0200

    add hometask41, 43

---
+++ hometask41/Program.cs
@@ -0,0 +1,17 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел.
+// Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// Ввод чисел останавливается при помощи ввода слова "stop"
+
+Console.WriteLine("Введите число");
+string a = "0";
+int count = 0;
+while(a != "stop")
+{
+    if(Convert.ToInt32(a) > 0)
+    {
+        count = count + 1;
+    }
+    a = Console.ReadLine();
+    
+}
+Console.WriteLine(count);
---
+++ hometask43/Program.cs
@@ -0,0 +1,22 @@
+﻿// Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых, 
+// заданных уравнениями y = k1 * x + b1, y = k2 * x + b2;
+// значения b1, k1, b2 и k2 задаются пользователем.
+
+double ReadLine(string msg)
+{
+    Console.WriteLine(msg);
+    double number = Convert.ToInt32(Console.ReadLine());
+    return number;
+}
+double b1 = ReadLine("Введите число для b1");
+double k1 = ReadLine("Введите число для k1");
+double b2 = ReadLine("Введите число для b2");
+double k2 = ReadLine("Введите число для k2");
+if(k1 == k2)
+{
+    Console.WriteLine("Прямые параллельны");
+}
+else
+{
+    Console.WriteLine($"Точка пересечения: ({(b2 - b1) / (k1 - k2)};{(k1 * b2 - k2 * b1) / (k1 - k2)})");
+}
